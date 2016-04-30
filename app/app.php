28b10aef4abd249a03089cb9de8dd7ecabe6c28f
Fallenstedt <a.fallenstedt@gmail.com>
2016-04-29 20:44:22 -0700

    remove ob_start

--- app/app.php
+++ app/app.php
@@ -1,6 +1,6 @@
 <?php
-    ob_start();
-    //turn output buffering on
+    // ob_start();
+    // //turn output buffering on
 
     date_default_timezone_set('America/Los_Angeles');
     require_once __DIR__."/../vendor/autoload.php";

0338795a523fcfa870aad44958ebee8e99f513a3
Fallenstedt <a.fallenstedt@gmail.com>
2016-04-29 20:40:50 -0700

    add output buffering and default timezone

--- app/app.php
+++ app/app.php
@@ -1,9 +1,13 @@
 <?php
+    ob_start();
+    //turn output buffering on
+
+    date_default_timezone_set('America/Los_Angeles');
     require_once __DIR__."/../vendor/autoload.php";
 
     $app = new Silex\Application();
     $app->register(new Silex\Provider\TwigServiceProvider(), array(
-    'twig.path' => __DIR__.'/../views'
+        'twig.path' => __DIR__.'/../views'
     ));
 
     $app->get("/", function() use ($app) {
